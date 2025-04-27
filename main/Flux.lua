a678c075e3a38e43d3031ad1260bd022dc31f819
Jayver-Dev <jaydenmiller290@gmail.com>
2025-04-26 21:02:22 -0500

    Update Flux.lua

--- main/Flux.lua
+++ main/Flux.lua
@@ -19,7 +19,7 @@ Window:CreateHomeTab({
 })
 
 -- Create Tab
-local Tab = Window:CreateTab({ Name = "Main", Icon = "dashboard", ImageSource = "Material", ShowTitle = true })
+local Tab = Window:CreateTab({ Name = "Aiming", Icon = "my_location", ImageSource = "Material", ShowTitle = true })
 Tab:CreateSection("Aimbot")
 
 -- Services
