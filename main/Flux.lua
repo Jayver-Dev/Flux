29a72529d10a82115971e174f7c6b1bdef1b658c
Jayver-Dev <jaydenmiller290@gmail.com>
2025-04-27 13:01:57 -0500

    Update Flux.lua

--- main/Flux.lua
+++ main/Flux.lua
@@ -1029,8 +1029,6 @@ UserInputService.InputEnded:Connect(function(input, processed)
     end
 end)
 
-MovementTab:CreateSection("Bypasses")
-
 --// Luna UI Setup
 MovementTab:CreateToggle({
     Name = "Enable Fly",
